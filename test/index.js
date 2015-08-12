4e3d1a20866e635963135f78af4ba5c33c431b5c
jedireza <reza@akhavan.me>
2015-08-12 12:47:46 -0700

    hapi 9.x.x updates

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@
 var Code = require('code');
 var Hapi = require('hapi');
 var Lab = require('lab');
+var Vision = require('vision');
 var Visionary = require('../');
 
 // Declare internals
@@ -22,14 +23,17 @@ describe('register()', function () {
 
     it('registers a views manager', function (done) {
 
-        var views = {
-            engines: { 'html': require('handlebars') },
-            path: __dirname
+        var VisionaryPlugin = {
+            register: Visionary,
+            options: {
+                engines: { 'html': require('handlebars') },
+                path: __dirname
+            }
         };
 
         var server = new Hapi.Server();
         server.connection();
-        server.register({ register: Visionary, options: views }, function (err) {
+        server.register([Vision, VisionaryPlugin], function (err) {
 
             expect(err).to.not.exist();
 
@@ -40,24 +44,30 @@ describe('register()', function () {
 
             server.route({ method: 'GET', path: '/', handler: handler });
 
-            server.inject('/', function (res) {
+            server.initialize(function () {
 
-                expect(res.result).to.equal('<div>\n    <h1>hi</h1>\n</div>\n');
-                done();
+                server.inject('/', function (res) {
+
+                    expect(res.result).to.equal('<div>\n    <h1>hi</h1>\n</div>\n');
+                    done();
+                });
             });
         });
     });
 
     it('registers a views manager (string engine)', function (done) {
 
-        var views = {
-            engines: { 'html': 'handlebars' },
-            path: __dirname
+        var VisionaryPlugin = {
+            register: Visionary,
+            options: {
+                engines: { 'html': 'handlebars' },
+                path: __dirname
+            }
         };
 
         var server = new Hapi.Server();
         server.connection();
-        server.register({ register: Visionary, options: views }, function (err) {
+        server.register([Vision, VisionaryPlugin], function (err) {
 
             expect(err).to.not.exist();
 
@@ -68,10 +78,13 @@ describe('register()', function () {
 
             server.route({ method: 'GET', path: '/', handler: handler });
 
-            server.inject('/', function (res) {
+            server.initialize(function () {
 
-                expect(res.result).to.equal('<div>\n    <h1>hi</h1>\n</div>\n');
-                done();
+                server.inject('/', function (res) {
+
+                    expect(res.result).to.equal('<div>\n    <h1>hi</h1>\n</div>\n');
+                    done();
+                });
             });
         });
     });
